9690c02b33ff804cef17452eea19580099a43feb
MarcosYH <marcosdeimos18@gmail.com>
2023-05-16 20:56:26 +0200

    config port

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ const app = express();
 var corsOptions= {
     origin: "https://localhost:8081"
 };
-
+//add middleware for request
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true })); 
+
+// add simple route
+
+app.get("/",(req, res) => {
+    res.json({message: "Welcome to my api for authentication!"});
+
+});
+
+// set port listen for request
+const PORT =process.env.PORT ||8080;
+app.listen(PORT, () => {
+    console.log("server is running on port ${PORT}.");
+});
