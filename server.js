1fb815f61bed2db9743ce67b9f361077ea412d7a
MarcosYH <marcosdeimos18@gmail.com>
2023-05-16 20:34:41 +0200

    add app.use option

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+//import express body and cors
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+//create express app
+const app = express();
+
+//
+var corsOptions= {
+    origin: "https://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true })); 
